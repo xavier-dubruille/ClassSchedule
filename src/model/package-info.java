a497e9344659255698a9f0a17121ea7c72fde77c
Poulpix <Poulpix@geraldinecordaro.home>
2010-12-22 15:47:18 +0100

    rajout du package-info, et p-e une babiole ou deux

---
+++ src/model/package-info.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * This package represent the internal state of the application. 
+ * All the informations about the teachers, the courses, the class rooms are here.
+ * 
+ * It also contain the tools to build, save and interact with this "internal state",
+ * and the classes in this package are the only able to do so, in order to preserve 
+ * the "Model-View-Controller" architecture.
+ * 
+ */
+
+
+package model;
